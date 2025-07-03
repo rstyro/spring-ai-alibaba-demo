60d7382c9a1f5cb24827fc85a5a5d675b2f8d863
lrs <rishuai.lu@yijiuaviation.com>
2025-07-03 11:29:32 +0800

    大模型调用工具函数优化

--- spring-ai-alibaba-tool-call/src/main/java/top/lrshuai/ai/toolcall/controller/DemoController.java
+++ spring-ai-alibaba-tool-call/src/main/java/top/lrshuai/ai/toolcall/controller/DemoController.java
@@ -1,12 +1,20 @@
 package top.lrshuai.ai.toolcall.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.definition.ToolDefinition;
+import org.springframework.ai.tool.function.FunctionToolCallback;
+import org.springframework.ai.tool.method.MethodToolCallback;
+import org.springframework.ai.util.json.schema.JsonSchemaGenerator;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.lrshuai.ai.common.resp.R;
+import top.lrshuai.ai.toolcall.tool.function.WeatherFunction;
 import top.lrshuai.ai.toolcall.tool.method.WeatherTool;
+
+import java.lang.reflect.Method;
 
 /**
  * demo 工具调用
@@ -33,13 +41,27 @@ public class DemoController {
     /**
      * 调用 函数工具
      */
-    @GetMapping("/askCall")
-    public R askCall(@RequestParam(value = "question", defaultValue = "深圳今天的天气怎样") String question) {
+    @GetMapping("/askFunction")
+    public R askFunction(@RequestParam(value = "question", defaultValue = "深圳今天的天气怎样") String question) {
         /**
          * 在调用 ChatClient 时，通过.toolNames() 传递函数工具的 Bean 名称，
          * 或者在实例化 ChatClient 对象的时候通过 .defalutToolNames() 方法传递函数工具
          */
         return R.ok(chatClient.prompt(question).toolNames("weatherFunction").call().content());
+    }
+
+    /**
+     * 开发者也可以不用定义 Bean，直接定义 FunctionToolCallBack 对象，在调用 ChatClient 时通过 .toolCallBacks()
+     * 或者在实例化 ChatClient 对象的时候通过 .defalutToolCallBacks() 传递 FunctionToolCallBack 对象：
+     */
+    @GetMapping("/askFunctionCallback")
+    public R askFunctionCallback(@RequestParam(value = "question", defaultValue = "深圳今天的天气怎样") String question) {
+        FunctionToolCallback<WeatherFunction.WeatherRequest, String> functionToolCallback = FunctionToolCallback
+                .builder("functionCallback", new WeatherFunction())
+                .description("获取城市今天的天气")
+                .inputType(WeatherFunction.WeatherRequest.class)
+                .build();
+        return R.ok(chatClient.prompt(question).toolCallbacks(functionToolCallback).call().content());
     }
 
     /**
@@ -54,6 +76,33 @@ public class DemoController {
         return R.ok(chatClient.prompt(question)
                 // 方法工具调用
                 .tools(new WeatherTool())
+                .call().content());
+    }
+
+    /**
+     * 调用 方法工具
+     */
+    @GetMapping("/askByToolCallback")
+    public R askByToolCallback(@RequestParam(value = "question", defaultValue = "深圳今天的天气怎样") String question) {
+        /**
+         * 可以使用 JsonSchemaGenerator.generateForMethodInput(method) 方法获取 Input Schema。
+         * 在调用 ChatClient 时，通过.toolCallbacks() 传递 MethodToolCallBack 对象，
+         * 或者在实例化 ChatClient 对象的时候通过 .defalutToolCallBacks() 方法传递工具对象：
+         */
+        Method method = ReflectionUtils.findMethod(WeatherTool.class, "geCityWeatherInfo", String.class);
+        String inputSchema = JsonSchemaGenerator.generateForMethodInput(method);
+        MethodToolCallback toolCallback = MethodToolCallback.builder()
+                .toolDefinition(ToolDefinition.builder()
+                        .description("获取城市天气信息")
+                        .name("geCityWeatherInfo")
+                        .inputSchema(inputSchema)
+                        .build())
+                .toolMethod(method)
+                .toolObject(new WeatherTool())
+                .build();
+        return R.ok(chatClient.prompt(question)
+                // 方法工具调用
+                .toolCallbacks(toolCallback)
                 .call().content());
     }
 
--- spring-ai-alibaba-tool-call/src/main/java/top/lrshuai/ai/toolcall/tool/method/WeatherTool.java
+++ spring-ai-alibaba-tool-call/src/main/java/top/lrshuai/ai/toolcall/tool/method/WeatherTool.java
@@ -4,12 +4,21 @@ import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
 
 /**
- * 方法工具
+ * 天气方法工具
  */
 public class WeatherTool {
 
     @Tool(description = "获取城市天气信息")
     public String getWeatherInfo(@ToolParam(description = "城市名称") String city) {
+        // todo 请求具体的接口得到城市天气
         return String.format("%s 天气是多云转晴，温度38.9",city);
+    }
+
+    /**
+     * 没有注解，用与toolCallback回调
+     */
+    public String geCityWeatherInfo(String city) {
+        // todo 请求具体的接口得到城市天气
+        return String.format("%s 天气是小雨多云，微风清爽",city);
     }
 }
